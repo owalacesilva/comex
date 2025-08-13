1d34ec82298a6303bf3e75def73bf875101c9c2e
Walace Silva <wsilva.emp@gmail.com>
2025-08-12 23:39:18 -0300

    feat: new files

---
+++ src/Domain/Entities/Entity.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Domain\Entities;
+
+/**
+ * Represents an abstract base class for an entity with a unique identifier.
+ */
+abstract class Entity
+{
+    protected null|string|int $id;
+
+    protected function __construct(null|string|int $id) {
+        $this->id = $id;
+    }
+
+    public function getId(): null|string|int {
+        return $this->id;
+    }
+
+    public function setId(null|string|int $id): void {
+        $this->id = $id;
+    }
+}
